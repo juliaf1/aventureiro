47cf6beac3e2f06fa1789ba8c507e7102bdc1a4d
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-29 01:12:35 -0300

    devise gem installed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  host = 'aventureiroilhagrande.herokuapp.com'
+
+  config.action_mailer.default_url_options = { host: host }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
