2045b2fdce5c89bac4a3e123e693e47e2f69313c
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-30 15:14:23 -0300

    validations added to models

--- app/models/campsite_reservation.rb
+++ app/models/campsite_reservation.rb
@@ -1,4 +1,7 @@
 class CampsiteReservation < ApplicationRecord
   belongs_to :user
   belongs_to :campsite
+
+  validates: :check_in, :check_out, :description, :number_guests, presence: true
+  validates: :description, length: { minimum: 30 }
 end
