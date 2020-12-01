88581a2d2fde874f8c6e8cbd274b678aa2719eaa
Tomas Gomes <tvb.gomes@gmail.com>
2020-12-01 20:31:08 -0300

    finished campsite_reservation actions, including destroy and index

--- app/controllers/campsite_reservations_controller.rb
+++ app/controllers/campsite_reservations_controller.rb
@@ -1,4 +1,6 @@
 class CampsiteReservationsController < ApplicationController
+  before_action :find_campsite, only: :new
+
   def index
     @campsite_reservations = CampsiteReservation.all
     authorize current_user, policy_class: CampsiteReservationPolicy
@@ -6,36 +8,49 @@ class CampsiteReservationsController < ApplicationController
 
   def new
     @campsite_reservation = CampsiteReservation.new
-    @campsite = Campsite.find(params[:campsite_id])
     authorize @campsite_reservation
   end
 
   def create
     @campsite_reservation = CampsiteReservation.new(reservation_params)
-    @campsite_reservation.user = current_user
-    @campsite_reservation.campsite_id = params[:campsite_id]
-    @campsite_reservation.status = 0
-    @campsite_reservation.total_price = @campsite_reservation.number_guests * @campsite_reservation.campsite.daily_price
+    set_user_campsite_and_status
+    set_total_price
     authorize @campsite_reservation
     if @campsite_reservation.save
-      redirect_to user_campsite_reservations_path(current_user), notice: "Your campsite reservation request was sent to:
-       #{@campsite_reservation.campsite.user.first_name}, please await the confirmation in the following days."
+      redirect_to user_campsite_reservations_path(current_user), notice: "Your campsite reservation request is pending confirmation
+      from #{@campsite_reservation.campsite.user.first_name}"
     else
-      render :new, notice: @campsite_reservation.errors.messages.to_s
+      render :new
     end
   end
 
   def destroy
     @campsite_reservation = CampsiteReservation.find(params[:id])
 
-    if @campsite_reservation.check_in - Date.today > 86_400
+    if @campsite_reservation.check_in - Date.today > 1
+      @campsite_reservation.destroy
       redirect_to user_campsite_reservations_path(current_user), notice: "Your reservation was cancelled"
     else
-      redirect_to user_campsite_reservations_path(current_user), notice: "You can't cancel your reservation 24h previous to the check-in time"
+      redirect_to user_campsite_reservations_path(current_user), notice: "You can't cancel your reservation 24h before arrival"
     end
   end
 
   private
+
+  def find_campsite
+    @campsite = Campsite.find(params[:campsite_id])
+  end
+
+  def set_user_campsite_and_status
+    @campsite_reservation.user = current_user
+    @campsite_reservation.campsite = find_campsite
+    @campsite_reservation.status = 0
+  end
+
+  def set_total_price
+    daily_price = @campsite_reservation.number_guests * @campsite_reservation.campsite.daily_price
+    @campsite_reservation.total_price = daily_price * (@campsite_reservation.check_out - @campsite_reservation.check_in).to_i
+  end
 
   def reservation_params
     params.require(:campsite_reservation).permit(:check_in, :check_out, :description, :number_guests)
--- app/models/campsite_reservation.rb
+++ app/models/campsite_reservation.rb
@@ -4,4 +4,5 @@ class CampsiteReservation < ApplicationRecord
   enum status: { pending: 0, confirmed: 1, rejected: 2 }
   validates :check_in, :check_out, :description, :number_guests, presence: true
   validates :description, length: { minimum: 30 }
+  validates_uniqueness_of :check_in, scope: :user_id
 end
