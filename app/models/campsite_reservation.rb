cdec77f8edb8bb651eb2fea0b372e010222836a7
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-30 23:11:35 -0300

    declared the enum hashes on the boat journey reservation and campsite reservation models

--- app/models/campsite_reservation.rb
+++ app/models/campsite_reservation.rb
@@ -1,7 +1,7 @@
 class CampsiteReservation < ApplicationRecord
   belongs_to :user
   belongs_to :campsite
-
+  enum status: { pending: 0, confirmed: 1, rejected: 2 }
   validates :check_in, :check_out, :description, :number_guests, presence: true
   validates :description, length: { minimum: 30 }
 end
