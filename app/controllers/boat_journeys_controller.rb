4344a712f4111ef35fe7ee65b7bd189372e427d4
Rowan Douglas <rowan.douglas29@gmail.com>
2020-12-02 13:48:16 -0300

    basic filter by date complete

--- app/controllers/boat_journeys_controller.rb
+++ app/controllers/boat_journeys_controller.rb
@@ -3,6 +3,9 @@ class BoatJourneysController < ApplicationController
 
   def index
     @boat_journeys = BoatJourney.all
+    @boat_journeys = @boat_journeys.select do |boat_journey|
+      boat_journey.departure_time.to_date == params[:date].to_date
+    end
+    @date = params[:date]
   end
-
 end
---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,6 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist//themes/dark.css"
+
+flatpickr(".datepicker", {
+  altInput: true
+});
