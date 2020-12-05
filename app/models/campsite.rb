f82b93b3a4a3f7c530eb224544a2756c7b2b0110
Rowan Douglas <rowan.douglas29@gmail.com>
2020-12-05 15:57:06 -0300

    changes to campsite and user models - has_many and belongs_to

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,6 +1,7 @@
 class Campsite < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  has_many :campsite_reservations
 
   validates :daily_price, :name, :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :boats
   has_many :boat_journeys, through: :boats
-
   has_many :boat_journey_reservations
 
   has_many :campsite_reservations
   has_many :campsites, through: :campsite_reservations
+  has_many :campsites
 
   has_one_attached :photo
 
