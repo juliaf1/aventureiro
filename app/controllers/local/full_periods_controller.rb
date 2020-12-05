51d16f82533720691ac3e72e492ef8d2073ba139
Rowan Douglas <rowan.douglas29@gmail.com>
2020-12-05 18:16:52 -0300

    full_period new and create function complete

--- app/controllers/local/full_periods_controller.rb
+++ app/controllers/local/full_periods_controller.rb
@@ -1,14 +1,21 @@
 class Local::FullPeriodsController < ApplicationController
   before_action :find_full_period, only: [ :destroy ]
-  before_action :find_campsite, only: [ :new ]
+  before_action :find_campsite, only: [ :new, :create ]
 
   def new
     @full_period = FullPeriod.new
   end
 
   def create
-    
     @full_period = FullPeriod.new(full_period_params)
+    @full_period.campsite = @campsite
+    
+    if @full_period.save
+      
+      redirect_to local_campsites_path
+    else
+      render :new
+    end
   end
 
   def destroy
--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,7 +1,7 @@
 class Campsite < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
-  has_many :full_periods
+  has_many :full_periods, dependent: :destroy
 
   validates :daily_price, :name, :description, presence: true
 end
