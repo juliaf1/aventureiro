30a8f84f4c341c0c445100022efe997a85882632
Rowan Douglas <rowan.douglas29@gmail.com>
2020-12-07 17:38:20 -0300

    user can create a permit request

--- app/controllers/permit_requests_controller.rb
+++ app/controllers/permit_requests_controller.rb
@@ -9,7 +9,19 @@ class PermitRequestsController < ApplicationController
   end
 
   def create
-    
+    @permit_request = PermitRequest.new(permit_params)
+    @permit_request.user = current_user
+    if @permit_request.save
+      redirect_to campsites_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def permit_params
+    params.require(:permit_request).permit(:arrival, :departure)
   end
 
 end
