ed73c040021282e9b32d71f080c504a55abc6312
Julia Frederico <magalhaesjulia@id.uff.br>
2020-12-09 11:09:42 -0300

    defined total price with before save on model, instead of controller

--- app/models/boat_journey_reservation.rb
+++ app/models/boat_journey_reservation.rb
@@ -2,5 +2,11 @@ class BoatJourneyReservation < ApplicationRecord
   belongs_to :user
   belongs_to :boat_journey
   enum status: { pending: 0, confirmed: 1, rejected: 2 }
-  validates :number_passengers, :total_price, presence: true
+  validates :number_passengers, presence: true
+
+  before_save :set_total_price
+
+  def set_total_price
+    self.total_price = number_passengers * boat_journey.price_person
+  end
 end
