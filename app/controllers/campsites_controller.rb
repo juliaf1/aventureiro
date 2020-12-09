18f654affe0438f1226d80f8714a45865556b9c8
Rowan Douglas <rowan.douglas29@gmail.com>
2020-12-09 14:07:54 -0300

    search function for campsites by date implemented

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -4,10 +4,39 @@ class CampsitesController < ApplicationController
   def index
     @campsites = Campsite.includes(:user)
     authorize @campsites
+
+    if params[:start_date]
+      @start_date = params[:start_date].split('to').first.to_date
+    end
+
+    if params[:end_date]
+      @end_date = params[:end_date].to_date
+    end
+    
+    if @end_date
+      @campsites = @campsites.select do |campsite|
+        confirm_check_in(campsite) && confirm_check_out(campsite)
+      end
+    end
+
   end
 
   def show
     @campsite = Campsite.find(params[:id])
     authorize @campsite
+  end
+
+  private
+
+  def confirm_check_in(campsite)
+    campsite.full_periods.all? do |period|
+      period.end_date < @start_date || period.start_date > @start_date
+    end
+  end
+
+  def confirm_check_out(campsite)
+    campsite.full_periods.all? do |period|
+      period.end_date < @end_date || period.start_date > @end_date
+    end
   end
 end
