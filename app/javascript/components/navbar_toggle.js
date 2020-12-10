1e4785c630944a5f1c774902746408ccb5e4e618
Julia Frederico <magalhaesjulia@id.uff.br>
2020-12-10 12:08:34 -0300

    icon toggles navbar on home page

--- app/javascript/components/navbar_toggle.js
+++ app/javascript/components/navbar_toggle.js
@@ -1,5 +1,6 @@
 const initNavbarToggle = () => {
   const localIcon = document.querySelector('.local-navbar-toggle');
+  const localIconHome = document.querySelector('.home-local-navbar-toggle');
   const localNavbar = document.querySelector('.local-navbar');
   const localNavbarHome = document.querySelector('.home-local-navbar');
   let current_rotation = 0;
@@ -9,9 +10,14 @@ const initNavbarToggle = () => {
       current_rotation += 360;
       localIcon.style.transform = 'rotate(' + current_rotation + 'deg)';
       localNavbar.classList.toggle('navbar-hidden');
-      localNavbarHome.classList.toggle('home-navbar-hidden');
     });
-  };
+  } else if (localIconHome) {
+    localIconHome.addEventListener('click', (event) => {
+      current_rotation += 360;
+      localIconHome.style.transform = 'rotate(' + current_rotation + 'deg)';
+      localNavbarHome.classList.toggle('navbar-hidden');
+    });
+  }
 }
 
 export { initNavbarToggle }
